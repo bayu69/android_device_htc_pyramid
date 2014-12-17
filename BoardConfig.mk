60f46941302c239cb480b1d1ab5ca2ad439bb261
Ivan Grinko <iivanich@gmail.com>
2014-12-17 15:58:05 +0200

    pyramid: Add device specific SElinux policies

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,9 @@ BOARD_USES_LEGACY_MMAP := true
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += \
+    device/htc/pyramid/sepolicy
+
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_VOLD_MAX_PARTITIONS := 36
